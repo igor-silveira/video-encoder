e6fc3e8e26ad743507ead14bcf74d6d0f31213b7
igor.silveira <igor-silveira10@hotmail.com>
2022-03-04 23:05:57 -0300

    feat: add video_id as job foreign key

--- framework/database/db.go
+++ framework/database/db.go
@@ -56,6 +56,7 @@ func (d *Database) Connect() (*gorm.DB, error) {
 	}
 	if d.AutoMigrateDb {
 		d.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
+		d.Db.Model(domain.Job{}).AddForeignKey("video_id", "videos (id)", "CASCADE", "CASCADE")
 	}
 
 	return d.Db, nil
