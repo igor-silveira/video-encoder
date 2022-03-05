a329674189990eac89231edca4f033e001279385
igor.silveira <igor-silveira10@hotmail.com>
2022-03-04 23:03:06 -0300

    chore: add orm tags

--- domain/video.go
+++ domain/video.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Video struct {
-	ID         string    `json:"encoded_video_folder" valid:"uuid"`
-	ResourceID string    `json:"resource_id" valid:"notnull"`
-	FilePath   string    `json:"file_path" valid:"notnull"`
+	ID         string    `json:"encoded_video_folder" valid:"uuid" gorm:"type:uuid;primary_key"`
+	ResourceID string    `json:"resource_id" valid:"notnull" gorm:"type:varchar(255)"`
+	FilePath   string    `json:"file_path" valid:"notnull" gorm:"type:varchar(255)"`
 	CreatedAt  time.Time `json:"-" valid:"-"`
-	Jobs       []*Job    `json:"-" valid:"-"`
+	Jobs       []*Job    `json:"-" valid:"-" gorm:"ForeignKey:VideoID"`
 }
 
 func init() {
