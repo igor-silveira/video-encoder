829edacd93502c42333d8437a6318eadbbef8ab8
igor.silveira <igor-silveira10@hotmail.com>
2022-03-27 19:56:13 -0300

    feat: add video repository test

---
+++ application/repositories/video_repository_test.go
@@ -0,0 +1,30 @@
+package repositories_test
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+	"video-enconder-microsservice/application/repositories"
+	"video-enconder-microsservice/domain"
+	"video-enconder-microsservice/framework/database"
+)
+
+func TestNewVideoRepositoryDbInsert(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	v, err := repo.Find(video.ID)
+
+	require.NotEmpty(t, v.ID)
+	require.Nil(t, err)
+	require.Equal(t, v.ID, video.ID)
+}
