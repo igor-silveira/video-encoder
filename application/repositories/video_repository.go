87143e5ddb81cba5353f2dec0a04decdc612afe5
igor.silveira <igor-silveira10@hotmail.com>
2022-03-27 20:06:15 -0300

    chore: preload Jobs relation

--- application/repositories/video_repository.go
+++ application/repositories/video_repository.go
@@ -36,7 +36,7 @@ func (repo VideoRepositoryDb) Insert(video *domain.Video) (*domain.Video, error)
 
 func (repo VideoRepositoryDb) Find(id string) (*domain.Video, error) {
 	var video domain.Video
-	repo.Db.First(&video, "id = ?", id)
+	repo.Db.Preloads("Jobs").First(&video, "id = ?", id)
 
 	if video.ID == "" {
 		return nil, fmt.Errorf("video does not exist")
