730290ca265af2f96c66ad9d028f8bc62cd171f2
igor.silveira <igor-silveira10@hotmail.com>
2022-04-02 20:14:32 -0300

    chore: add video service test

---
+++ application/services/video_service_test.go
@@ -0,0 +1,49 @@
+package services_test
+
+import (
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"log"
+	"testing"
+	"time"
+	"video-enconder-microsservice/application/repositories"
+	"video-enconder-microsservice/application/services"
+	"video-enconder-microsservice/domain"
+	"video-enconder-microsservice/framework/database"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepositoryDb) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "sample.mp4"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+
+	return video, repo
+}
+
+func TestVideoServiceDownload(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("bucket-for-study")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+}
