f6740a3676e46ea58e3986ab0f99bfff4ed3de8a
igor.silveira <igor-silveira10@hotmail.com>
2022-04-02 18:24:15 -0300

    chore: add video service

---
+++ application/services/video_service.go
@@ -0,0 +1,62 @@
+package services
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"io/ioutil"
+	"log"
+	"os"
+	"video-enconder-microsservice/application/repositories"
+	"video-enconder-microsservice/domain"
+)
+
+type VideoService struct {
+	Video           *domain.Video
+	VideoRepository repositories.VideoRepository
+}
+
+func NewVideoService() VideoService {
+	return VideoService{}
+}
+
+func (v *VideoService) Download(bucketName string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	bkt := client.Bucket(bucketName)
+	obj := bkt.Object(v.Video.FilePath)
+	r, err := obj.NewReader(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	defer r.Close()
+
+	body, err := ioutil.ReadAll(r)
+
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4")
+
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(body)
+
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	log.Printf("video %v has been stored", v.Video.ID)
+
+	return nil
+}
