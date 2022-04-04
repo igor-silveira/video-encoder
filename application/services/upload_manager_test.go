5d0f0e1e41e391ca8faddab2b5902fd8dbb7de48
igor.silveira <igor-silveira10@hotmail.com>
2022-04-04 19:41:41 -0300

    chore: add upload manager test

---
+++ application/services/upload_manager_test.go
@@ -0,0 +1,44 @@
+package services_test
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/require"
+	"log"
+	"os"
+	"testing"
+	"video-enconder-microsservice/application/services"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func TestVideoServiceUpload(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("bucket-for-study")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+	require.Nil(t, err)
+
+	videoUpload := services.NewVideoUpload()
+	videoUpload.OutputBucket = "bucket-for-study"
+	videoUpload.VideoPath = os.Getenv("LOCAL_STORAGE_PATH") + "/" + video.ID
+
+	doneUpload := make(chan string)
+	go videoUpload.ProcessUpload(50, doneUpload)
+
+	result := <-doneUpload
+	require.Equal(t, result, "upload completed")
+}
